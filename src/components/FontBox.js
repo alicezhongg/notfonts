7758f1fe8dfece4813cb0e402dd9d5fb0df47f88
alicezhongg <allie.y.zhang@gmail.com>
2023-11-18 21:44:40 -0600

    initial commit

---
+++ src/components/FontBox.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FontBox = (props) => {
+  return (
+    <div className="border bg-[#000000] text-white text-8xl">
+      <h1>{props.name}</h1>
+    </div>
+  );
+};
+
+export default FontBox;
