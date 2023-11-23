b37c3426b734f73f464c89afc668bf4679971301
alicezhongg <allie.y.zhang@gmail.com>
2023-11-23 00:06:46 -0600

    fixed layout of home page

--- src/components/FontBox.js
+++ src/components/FontBox.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const FontBox = (props) => {
   return (
-    <div className="border bg-[#000000] text-white text-8xl">
+    <div className="border text-[#0f0800] text-8xl">
       <h1>{props.name}</h1>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,17 @@ import React from "react";
 import FontBox from "@/components/FontBox";
 
 const IndexPage = () => {
-  const [fonts, setFonts] = React.useState(["Karla", "Comic Sans MS", "Inter", "ITC Garamond", "Roboto", "Times New Roman", "Verdana"]);
+  const [fonts, setFonts] = React.useState(["Karla", "ZCOOL Kuai Le", "ZCool QingKe HuangYou", "Alice ", "Playfair Display", "Times New Roman", "Verdana"]);
 
   return (
     <>
       <div>
-        <h1 className="text-6xl text-center">NOTFONTS</h1>
-        <p>they're called typefaces</p>
-
+        <h1 className="text-9xl text-center mt-[20rem] mb-3">NOTFONTS</h1>
+        <p className="text-3xl text-center mb-6">THEY'RE CALLED TYPEFACES</p>
+        <div class="flex justify-center space-x-4 m-2">
+          <p className="text-6xl text-center border">Browse</p> 
+          <p className="text-6xl text-center border" >Detect</p>
+        </div>
         {fonts.map((font) => (
           <FontBox key={font} name={font} />
         ))}
