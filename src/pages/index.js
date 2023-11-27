e97fd331c67b5a4be22523808a2fc48519a90c10
alicezhongg <allie.y.zhang@gmail.com>
2023-11-27 17:34:43 -0600

    banner & pushed text up

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,25 +12,39 @@ const IndexPage = () => {
       </div>
       <div className="font-Karla font-regular flex items-center justify-center h-screen">
         <div className="align-middle">
-          <div className="flex justify-center">
+          <Image
+            src={"/logo_w_bkgd.png"}
+            alt="Picture of the Logo"
+            width={1920}
+            height={1080}
+          />
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[35%]">
+            <p className="text-3xl text-center text-[#265479]">
+              Never know where to find typefaces? We got you covered!
+            </p>
+            <p className="font-light text-2xl text-center text-[#0F0800] pb-10">
+              You can even see examples of how they are used in real life!
+            </p>
+          </div>
+          {/* <div className="flex justify-center">
             <Image
               src="/logo.png"
               alt="Picture of the logo"
               width={800}
               height={300}
             />
-          </div>
+          </div> */}
           {/* <h1 className="text-9xl font-extrabold text-center text-[#265479] pb-3">
             NOTFONTS
           </h1> */}
           <div>
-            <p className="text-3xl text-center text-[#0F0800]">
+            {/* <p className="text-3xl text-center text-[#0F0800]">
               Never know where to find typefaces? We got you covered!
             </p>
             <p className="font-light text-2xl text-center text-[#0F0800] pb-10">
               You can even see examples of how they are used in real life!
-            </p>
-            <div className="flex justify-center space-x-10 m-2">
+            </p> */}
+            <div className="flex justify-center space-x-10 pt-10 m-2">
               <div className="w-1/5">
                 <Link href={{ pathname: "/detect" }}>
                   <p className="text-6xl text-center p-2 rounded-lg text-[#265479] outline outline-[#E6E7E6] outline-2 hover:outline-2 hover:outline-offset-2 hover:outline-[#265479] hover:shadow-lg">
@@ -74,12 +88,12 @@ const IndexPage = () => {
         </div>
       </div>
       <div className="relative">
-        <Image
+        {/* <Image
               src={"/logo_w_bkgd.png"}
               alt="Picture of the Logo"
               width={1920}
               height={1080}
-            />
+            /> */}
       </div>
     </>
   );
